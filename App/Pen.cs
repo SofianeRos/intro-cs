e319b27f2d203f178e0d4440f2840440f1ab53c8
ES_PREPA_DEV <sosoros66@icloud.com>
2026-03-09 15:12:15 +0100

    premiere clase en cs

---
+++ App/Pen.cs
@@ -0,0 +1,12 @@
+namespace intro_cs.App;
+
+public class Pen
+{
+    protected string Color;
+
+    public Pen(string color)
+    {
+        this.Color = color;
+    }
+    
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
-﻿namespace intro_cs;
+﻿using intro_cs.App;
+
+namespace intro_cs;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+
+        var unStylo = new Pen("black");
+        Console.WriteLine(unStylo);
     }
 }
